2e11b35513f7bd6602da74f11c4684890ae11b21
dennis adrian <adriangbetancur@gmail.com>
2021-01-29 00:08:37 -0400

    webpack configuration is finally ready (I think)

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    fallback: {
+      fs: false,
+      net: false
+    },
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new NodePolyfillPlugin()],
+};
