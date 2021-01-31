1c47b12f7f443e8b96ae02b47189016ebf63f9c6
dennis adrian <adriangbetancur@gmail.com>
2021-01-31 12:08:20 -0400

    Port was changed just because

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const PORT = 8000;
+const PORT = 8001;
 app.get('/', (req,res) => res.send('Express + TypeScript Server. Jenkins is already linked.'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
