9d3001454644d87b6fcf5212ac884fc73ca52f3c
dennis adrian <adriangbetancur@gmail.com>
2021-02-01 14:04:25 -0400

    Minor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 const PORT = 8001;
-app.get('/', (req,res) => res.send('Express + TypeScript Server. Jenkins is already linked.'));
+app.get('/', (req,res) => res.send('Express + TypeScript Server. Jenkins is already linked with a Jenkinsfile'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
