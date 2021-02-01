c846c01dc0d83255616d73ecc8b27faa09f7f64d
dennis adrian <adriangbetancur@gmail.com>
2021-02-01 14:11:15 -0400

    Another minor change

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 const app = express();
 const PORT = 8001;
-app.get('/', (req,res) => res.send('Express + TypeScript Server. Jenkins is already linked with a Jenkinsfile'));
+const name = 'Dennis Adrian'
+app.get('/', (req,res) => res.send(`Express + TypeScript Server. Jenkins is already linked with a Jenkinsfile by ${name}`));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
