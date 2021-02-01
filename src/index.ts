01755ab58b52c6aaf3cd533f07345d8a1fe6a20d
dennis adrian <adriangbetancur@gmail.com>
2021-02-01 14:27:45 -0400

    Minor. date added to console

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,14 @@ import express from 'express';
 const app = express();
 const PORT = 8001;
 const name = 'Dennis Adrian'
+
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+let yyyy = today.getFullYear();
+
 app.get('/', (req,res) => res.send(`Express + TypeScript Server. Jenkins is already linked with a Jenkinsfile by ${name}`));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+  console.log(`The date is: ${dd}/${mm}/${yyyy}`);
 });
